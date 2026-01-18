96f0f05ea56792086eed7bb556b7cd7aea133e72
DRMDavid <ds6434743@gmail.com>
2026-01-18 02:22:37 -0600

    Revert "Add ImGuizmo and ImGui docking integration"
    This reverts commit a8807c77b34f653e1fedc2bbc0dd8caea0a59efb.

--- MinerEngine/Include/BaseApp.h
+++ MinerEngine/Include/BaseApp.h
@@ -14,10 +14,6 @@
 #include "SamplerState.h"
 #include "Model3D.h"
 #include "ECS/Actor.h"
-#include "GUI/GUI.h"
-
-extern IMGUI_IMPL_API
-LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class
 	BaseApp {
@@ -25,12 +21,6 @@ public:
 	BaseApp() = default;
 	~BaseApp() { destroy(); }
 
-<<<<<<< Updated upstream
-=======
-	HRESULT
-		awake();
-
->>>>>>> Stashed changes
 	int
 		run(HINSTANCE hInst, int nCmdShow);
 
@@ -67,11 +57,7 @@ private:
 	Buffer															m_cbNeverChanges;
 	Buffer															m_cbChangeOnResize;
 	//Buffer															m_cbChangesEveryFrame;
-<<<<<<< Updated upstream
 	Texture 														m_PrintstreamAlbedo;
-=======
-	Texture 														m_cyberGunAlbedo;
->>>>>>> Stashed changes
 	//SamplerState												m_samplerState;
 
 	//XMMATRIX                            m_World;
@@ -80,24 +66,13 @@ private:
 	//XMFLOAT4                            m_vMeshColor;// (0.7f, 0.7f, 0.7f, 1.0f);
 
 	std::vector<EU::TSharedPointer<Actor>> m_actors;
-<<<<<<< Updated upstream
 	EU::TSharedPointer<Actor> m_Printstream;
-=======
-	EU::TSharedPointer<Actor> m_cyberGun;
->>>>>>> Stashed changes
 
 
 	Model3D* m_model;
 
-<<<<<<< Updated upstream
 
 	CBChangeOnResize										cbChangesOnResize;
 	CBNeverChanges											cbNeverChanges;
 	//CBChangesEveryFrame									cb;
-=======
-	CBChangeOnResize										cbChangesOnResize;
-	CBNeverChanges											cbNeverChanges;
-	//CBChangesEveryFrame									cb;
-	GUI																m_gui;
->>>>>>> Stashed changes
 };
--- MinerEngine/Source/ECS/Actor.cpp
+++ MinerEngine/Source/ECS/Actor.cpp
@@ -17,9 +17,6 @@ Actor::Actor(Device& device) {
 		ERROR("Actor", classNameType.c_str(), "Failed to create new CBChangesEveryFrame");
 	}
 
-	// Awake
-	awake();
-
 	hr = m_sampler.init(device);
 	if (FAILED(hr)) {
 		ERROR("Actor", classNameType.c_str(), "Failed to create new SamplerState");
